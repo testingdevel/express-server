56ea293f4b1849f174c4b8cf703db9e5c5d4fb82
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-19 13:31:55 -0500

    Add express router with extra endpoints

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const router = express.Router()
 
-app.get('/docs', (req, res) => res.send('This is the docs page'))
+router.get('/shows', (req, res) => res.send('Shows'))
+router.get('/hosts', (req, res) => res.send('Hosts'))
+
+app.get('/', (req, res) => res.send('Welcome to the root route'))
+
+app.use('/api/v1', router)
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
