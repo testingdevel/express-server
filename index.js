a9873a41fe022ae95c6076a5105965d91aecb1d7
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-19 12:59:35 -0500

    Add /docs route

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/docs', (req, res) => res.send('This is the docs page'))
+
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
