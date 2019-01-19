a3a9dbee28de5c8763033eadb554665387ed12ad
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-19 12:52:42 -0500

    Update index.js and start script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-module.exports = app
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
