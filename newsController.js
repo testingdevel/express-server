dff588a41db6283e618c3c0183059ad66fb9b5d3
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-22 19:22:13 -0500

    Add top stories call

--- newsController.js
+++ newsController.js
@@ -1,10 +1,31 @@
 const express = require('express')
+const request = require('request-promise-native')
 
 const router = express.Router()
 
+
+const mapTopStories = (story) => {
+  return {
+    id: story.id,
+    title: story.title,
+    source: story.source,
+    sourceId: story.sourceId,
+    numViewers: story.typeAttributes.trending.numViewers,
+    image: story.typeAttributes.imageLarge
+  }
+}
+
+
 router.get('/top-stories', (req, res) => {
 
-  res.send('this is the /top-stories page')
+  return request({
+    uri: 'https://api-gw.radio-canada.ca/aggregate-content/v1/items?lineupSlug=trending-news',
+    json: true
+  })
+
+  .then(data => {
+    return res.send(data.map(mapTopStories))
+  })
 })
 
 module.exports = router
