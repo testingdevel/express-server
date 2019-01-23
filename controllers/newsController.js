db8e1614329a42f5e813c53819872b2e77f805a9
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-22 20:09:23 -0500

    Add /world endpoint in newsController

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -10,8 +10,26 @@ const mapTopStories = (story) => {
     title: story.title,
     source: story.source,
     sourceId: story.sourceId,
-    numViewers: story.typeAttributes.trending.numViewers,
-    image: story.typeAttributes.imageLarge
+    image: story.typeAttributes.imageLarge,
+    publishedAt: story.publishedAt,
+    readablePublishedAt: story.readablePublishedAt,
+    updatedAt: story.updatedAt,
+    readableUpdatedAt: story.readableUpdatedAt,
+    numViewers: story.typeAttributes.trending.numViewers
+  }
+}
+
+const mapWorldNews = (story) => {
+  return {
+    id: story.id,
+    title: story.title,
+    source: story.source,
+    sourceId: story.sourceId,
+    image: story.typeAttributes.imageLarge,
+    publishedAt: story.publishedAt,
+    readablePublishedAt: story.readablePublishedAt,
+    updatedAt: story.updatedAt,
+    readableUpdatedAt: story.readableUpdatedAt
   }
 }
 
@@ -25,6 +43,18 @@ router.get('/top-stories', (req, res) => {
 
   .then(data => {
     return res.send(data.map(mapTopStories))
+  })
+})
+
+router.get('/world', (req, res) => {
+
+  return request({
+    uri: 'https://api-gw.radio-canada.ca/aggregate-content/v1/categories/36/items',
+    json: true
+  })
+
+  .then(data => {
+    return res.send(data.map(mapWorldNews))
   })
 })
 
