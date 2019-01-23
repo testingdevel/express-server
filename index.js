a0ea937c80bad2a4ff349282a06927e869846161
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-22 19:39:18 -0500

    Move newsController to controllers folder, add folder to buildspec

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const newsController = require('./newsController')
+const newsController = require('./controllers/newsController')
 
 const router = express.Router()
 
