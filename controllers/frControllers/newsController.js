7d39ac28133b333e887604ba0851c3822349b0ef
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-01-30 18:11:09 -0500

    Add top stories request for french news

--- controllers/frControllers/newsController.js
+++ controllers/frControllers/newsController.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const request = require('request-promise-native')
 
+// in this implementation we are using prod neuro,
+// for the hackathon we might need this key if we are using
+// hackathon neuro
 const key = '31b2bb0e-85ec-4406-9b22-31c93d7e75f9'
 
 const mapNeuroNewsItem = (item) => {
+
+  // this check exists because a neuro article might not have an image
+  const imageUrl = item.summaryImage ? item.summaryImage.concreteImages[0].mediaLink.href : ''
+
   return {
     id: item.id,
     title: item.title,
     source: 'neuro',
     sourceId: item.id,
-    image: item.summaryImage.concreteImages[0].mediaLink.href,
+    image: imageUrl,
     publishedAt: null,
     readablePublishedAt: null,
     updatedAt: item.publishedLastTimeAt,
@@ -20,17 +27,17 @@ const mapNeuroNewsItem = (item) => {
 
 const router = express.Router()
 
-// router.get('/top-stories', (req, res) => {
-//
-//   return request({
-//     uri: `https://services.radio-canada.ca/neuro/v1/themes/1/lineup?client_key=${key}`,
-//     json: true
-//   })
-//
-//   .then(data => {
-//     return res.send(data.pagedList.items[0])
-//   })
-// })
+router.get('/top-stories', (req, res) => {
+
+  return request({
+    uri: 'https://services.radio-canada.ca/neuro/v1/future/lineups/4159',
+    json: true
+  })
+
+  .then(data => {
+    return res.send(data.pagedList.items.map(mapNeuroNewsItem))
+  })
+})
 
 router.get('/world', (req, res) => {
 
